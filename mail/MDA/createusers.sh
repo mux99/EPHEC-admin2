c9eef46e27fdab88e1afea1dea8e13eea2c2135a
maxime <mux99@live.be>
2023-03-21 18:35:40 +0100

    mail internal finished

--- mail/MDA/createusers.sh
+++ mail/MDA/createusers.sh
@@ -2,15 +2,13 @@
 
 USERS_FILE="/users.txt"
 
-while read -r line; do
+while read line; do
   username=$(echo "$line" | cut -d':' -f1)
   password=$(echo "$line" | cut -d':' -f2)
+  test=$(id -u "$username")
+  useradd -g vmail -m $username -p $(openssl passwd -1 $password)
 
-  useradd -m -s /bin/bash "$username"
-  echo "$username:$password" | chpasswd
-
-  mkdir -p /var/mail/"$username"
-
-  chown "$username:$username" /var/mail/"$username"
-  chmod 0700 /var/mail/"$username"
+  #mkdir -p /var/mail/"$username"
+  #chown "$username:$username" /var/mail/"$username"
+  #chmod 0700 /var/mail/"$username"
 done < "$USERS_FILE"
