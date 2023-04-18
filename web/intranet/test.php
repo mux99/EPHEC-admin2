711103c4089b3094d4b315030db87fd16150edce
MIKObscura <105145988+MIKObscura@users.noreply.github.com>
2023-04-18 16:56:55 +0200

    added basic database connectivity

--- web/intranet/test.php
+++ web/intranet/test.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<?php
+$servername = "db.woodytoys.lab";
+$username = "root";
+$password = "example";
+
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+?>
