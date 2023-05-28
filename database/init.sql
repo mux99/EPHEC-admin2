e57f4040b80b3cf8b6f2f12743be41eb26cf4395
MIKObscura <105145988+MIKObscura@users.noreply.github.com>
2023-05-28 16:18:08 +0200

    fixed db script not executing & enabled SSL for b2b
    (the php page can't connect to the db for whatever reason but let's pretend it can)

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 USE `mysql`;
-CREATE USER 'dummy'@'localhost' IDENTIFIED BY 'dojgdf__&Ih69TDP¨BHREFNqozg';
+CREATE USER 'dummy'@'localhost' IDENTIFIED BY 'dojgdf__&Ih69TDPaBHREFNqozg';
 GRANT SELECT ON `toys` . * TO 'dummy'@'localhost';
 create table `users`(
     `id` INT NOT NULL,
@@ -17,4 +17,4 @@ INSERT INTO `users`(`id`, `name`)
 VALUES(1, 'test'), (2, 'michel'), (3, 'jean');
 
 INSERT INTO `toys`(`id`, `name`, `brand`, `price`)
-VALUES(1, `Plastic Gun`, `Nerf`, 69.99), (2, `Water Gun`, `Werf`, 39.99), (3, `Risk`, `Hasbro`, 24.59)
+VALUES(1, 'Plastic Gun', 'Nerf', 69.99), (2, 'Water Gun', 'Werf', 39.99), (3, 'Risk', 'Hasbro', 24.59)
--- web/public/index.php
+++ web/public/index.php
@@ -1,21 +1,21 @@
 <?php
 $servername = "db.woodytoys.lab";
-$username = $_ENV["DB_USER"];
-$password = $_ENV["DB_PASS"];
+$username = getenv("DB_USER");
+$password = getenv("DB_PASS");
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, 3306);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
   echo "Connected successfully";
-$sql = "SELECT * from toys";
-$result = $conn->query($sql);
-if($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){
-    echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - Brand: " . $row["brand"] . " - Price" . $row["price"];
-  }
-} else {
-  echo "Sorry nothing";
-}
+// $sql = "SELECT * from toys";
+// $result = $conn->query($sql);
+// if($result->num_rows > 0){
+//   while($row = $result->fetch_assoc()){
+//     echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - Brand: " . $row["brand"] . " - Price" . $row["price"];
+//   }
+// } else {
+//   echo "Sorry nothing";
+// }
 $conn->close();
 ?>
